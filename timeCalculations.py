398432864ed0a5a77857475ff40e9cdf7866574a
James Van Dyk <jvandyk1@ivytech.edu>
2025-11-03 12:51:00 -0500

    Added 3.2 - 3.6

--- timeCalculations.py
+++ timeCalculations.py
@@ -1,5 +1,21 @@
+#3.1
 seconds = 60
 minutes = 60
-secInHour = seconds * minutes
-print(secInHour)
+
+#3.2
+seconds_per_hour = seconds * minutes
+print(seconds_per_hour)
 # Output: 3600
+
+#3.3
+hours = 24
+
+#3.4
+seconds_per_day = seconds_per_hour * hours
+
+#3.5
+print(seconds_per_day / seconds_per_hour)
+
+#3.6
+print(seconds_per_day // seconds_per_hour)
+
